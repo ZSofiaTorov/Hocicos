2c526214a12dd4ffbdc488bc8ad7e91061690b3c
SOFIAREP <131830444+ZSofiaTorov@users.noreply.github.com>
2024-09-14 00:46:46 -0500

    Prueba migracion tablas

---
+++ PERROS2/Migrations/20240914024149_AgregarNuevaEntidad.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PERROS2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarNuevaEntidad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PERROS2/Migrations/20240914025114_AgregarRelacionProductoSabores.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PERROS2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarRelacionProductoSabores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Productos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Sabores",
+                columns: table => new
+                {
+                    SaborID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductoID = table.Column<int>(type: "int", nullable: false),
+                    NombreDeSabor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContenidoDelColageno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CantidadEnStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sabores", x => x.SaborID);
+                    table.ForeignKey(
+                        name: "FK_Sabores_Productos_ProductoID",
+                        column: x => x.ProductoID,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sabores_ProductoID",
+                table: "Sabores",
+                column: "ProductoID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sabores");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Descripcion",
+                table: "Productos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
