0f6c67ef620b91884db5fe24d16cb2e9c14ea625
Omar Meneses <fernandomenesesda@gmail.com>
2024-09-13 21:58:12 -0500

    Se creó el model de envíos

---
+++ PERROS2/Models/Envios.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PERROS2.Models
+{
+    public class Envios
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EnvíoID { get; set; }
+
+        [Required]
+        public int PedidoID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string MétodoDeEnvío { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string DirecciónDeEnvío { get; set; }
+
+        public DateTime? FechaDeEnvío { get; set; }
+    }
+}
