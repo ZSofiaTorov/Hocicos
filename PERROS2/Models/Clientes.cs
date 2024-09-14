3224f5c2a6143bf53420ac38391e06c54395b5b5
Omar Meneses <fernandomenesesda@gmail.com>
2024-09-13 21:30:49 -0500

    Creación de modelo de clientes: 1

--- PERROS2/Models/Clientes.cs
+++ PERROS2/Models/Clientes.cs
@@ -1,7 +1,39 @@
-﻿namespace PERROS2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PERROS2.Models
 {
     public class Clientes
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClienteID { get; set; }
 
+        [Required]
+        [StringLength(255)]
+        public string NombreComercial { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TipoDeCliente { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string CorreoElectronico { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContraseñaHash { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Dirección { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Teléfono { get; set; }
+
+        public DateTime? FechaDeCreación { get; set; }
     }
 }
